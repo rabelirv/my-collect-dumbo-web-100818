ed5938f041195f798908d97cbc94b9402e34d2bd
Irving E Rabel <rabel606@gmail.com>
2018-10-02 00:32:16 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(array)
+   i = 0
+languages = []
+  while i < array.length
+  languages<< yield(array[i])
+  i = i + 1
+end
+languages
+end
 
-
+my_collect(['ruby', 'javascript', 'python', 'objective-c']){ |lang|lang.upcase}
+my_collect(['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']){|name| name.split(" ").first}
